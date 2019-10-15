f6257b17b8aa401cd2bf1998a3d0fabc99f4397b
Tony Tea <tonybountea@gmail.com>
2019-10-14 22:44:40 -0600

    Updates

---
+++ precache-manifest.71254ac7833cb41eb13f91625e2453cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b76bee03fec133280ad40e2808f41e2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "99597512f1effe6aa3f0",
+    "url": "/my-app/static/css/main.aeb7a8bd.chunk.css"
+  },
+  {
+    "revision": "7545a03fa8d462cc3f61",
+    "url": "/my-app/static/js/2.4838dd46.chunk.js"
+  },
+  {
+    "revision": "99597512f1effe6aa3f0",
+    "url": "/my-app/static/js/main.a3665377.chunk.js"
+  },
+  {
+    "revision": "37335ddf11fdd0cc5981",
+    "url": "/my-app/static/js/runtime-main.ed6f54cb.js"
+  },
+  {
+    "revision": "54413459a5adf3f82686db50595cba5a",
+    "url": "/my-app/static/media/BebasNeue-Regular.54413459.ttf"
+  },
+  {
+    "revision": "55ae54006a29156b841eb25354d2e095",
+    "url": "/my-app/static/media/CS-portfolio-background.55ae5400.jpg"
+  }
+]);
